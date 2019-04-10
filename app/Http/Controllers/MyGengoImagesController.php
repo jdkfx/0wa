7247938593cf57d38b41ad82769fd5f438484106
haruki.tazoe <amateurranch@gmail.com>
2019-04-10 12:03:06 +0000

    素材画像＆合成画像の保存先を変更

--- app/Http/Controllers/MyGengoImagesController.php
+++ app/Http/Controllers/MyGengoImagesController.php
@@ -37,9 +37,9 @@ class MyGengoImagesController extends Controller
         $createdImg = $this->change_per_strlen($inToImg);
         
         $filename = \Illuminate\Support\Str::random(40);
-        $createdImg->save(public_path() . '/storage/'. $filename . '.jpg');
+        $createdImg->save(public_path() . '/' . $filename . '.jpg');
         
-        $myGengoImage->createdImg = '/storage/' . $filename . '.jpg';
+        $myGengoImage->createdImg = $filename . '.jpg';
         $myGengoImage->save();
         
         return view('mygengo.complete',[
@@ -52,12 +52,12 @@ class MyGengoImagesController extends Controller
     // 最大は四文字
     public function change_per_strlen($inToImg){
         $strlen = mb_strlen($inToImg);
-        $backImg = public_path().'/storage/backimg.jpg';
+        $backImg = public_path() . '/backimg.jpg';
         
         switch($strlen){
             case 1:
                 $createdImg = Image::make($backImg)->text($inToImg, 355, 340, function($font) {
-                    $font->file(public_path().'/fonts/hkgokukaikk.ttf');
+                    $font->file(public_path() . '/fonts/hkgokukaikk.ttf');
                     $font->size(200);
                     $font->align('center');
                     $font->color('#000000');
@@ -68,7 +68,7 @@ class MyGengoImagesController extends Controller
             case 2:
                 $strarray = $this->vertical_writing(2,$inToImg);
                 $createdImg = Image::make($backImg)->text($strarray, 360, 400, function($font) {
-                    $font->file(public_path().'/fonts/hkgokukaikk.ttf');
+                    $font->file(public_path() . '/fonts/hkgokukaikk.ttf');
                     $font->size(130);
                     $font->align('center');
                     $font->color('#000000');
@@ -78,7 +78,7 @@ class MyGengoImagesController extends Controller
             case 3:
                 $strarray = $this->vertical_writing(3,$inToImg);
                 $createdImg = Image::make($backImg)->text($strarray, 360, 385, function($font) {
-                    $font->file(public_path().'/fonts/hkgokukaikk.ttf');
+                    $font->file(public_path() . '/fonts/hkgokukaikk.ttf');
                     $font->size(80);
                     $font->align('center');
                     $font->color('#000000');
@@ -88,7 +88,7 @@ class MyGengoImagesController extends Controller
             case 4:
                 $strarray = $this->vertical_writing(4,$inToImg);
                 $createdImg = Image::make($backImg)->text($strarray, 355, 390, function($font) {
-                    $font->file(public_path().'/fonts/hkgokukaikk.ttf');
+                    $font->file(public_path() . '/fonts/hkgokukaikk.ttf');
                     $font->size(60);
                     $font->align('center');
                     $font->color('#000000');
